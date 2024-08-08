e8061defa794d70bf7928ff5894cd0e78105e072
amenonegames <amenone.games@gmail.com>
2024-08-08 21:10:46 +0900

    test case

---
+++ Assets/TempSave.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using MemoryPack;
+using UnityEngine;
+
+namespace Temporary
+{
+    public class TempSave : MonoBehaviour
+    {
+        private void Start()
+        {
+            var v = new Person { Age = 40, Name = "John" };
+
+            var bin = MemoryPackSerializer.Serialize(v);
+            var val = MemoryPackSerializer.Deserialize<Person>(bin);
+
+            Debug.Log(val.Name + " " + val.Age);
+        }
+    }
+    
+    [MemoryPackable]
+    public partial class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        // static partial void StaticConstructor()
+        // {
+        //     Debug.Log(" run static constructor");
+        // }
+    }
+}
+
+
